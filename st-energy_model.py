60dc4224b9115a2a725ca2f4689df918e23de1b2
James O'Reilly <44675992+joreilly86@users.noreply.github.com>
2023-06-23 16:58:18 -0700

    Update st-energy_model.py
    Formatted the outputs

--- st-energy_model.py
+++ st-energy_model.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('Energy Model App')
+st.title('Pumped Storage Hydropower Energy Model App')
 
 st.markdown("""
 This app calculates the available energy (in MWh) in a hypothetical reservoir system. 
@@ -15,12 +15,12 @@ water_density = 1000  # kg/m³
 gravity = 9.81  # m/s²
 
 # get inputs from the user
-upper_reservoir_level = st.number_input('Upper Reservoir Level (m)', value=0.0)  # meters
-lower_reservoir_level = st.number_input('Lower Reservoir Level (m)', value=0.0)  # meters
-upper_reservoir_volume = st.number_input('Upper Reservoir Volume (cubic meters)', value=0.0)  # cubic meters
-lower_reservoir_volume = st.number_input('Lower Reservoir Volume (cubic meters)', value=0.0)  # cubic meters
-pump_efficiency = st.number_input('Pump Efficiency (decimal)', value=0.0)  # decimal
-generator_efficiency = st.number_input('Generator Efficiency (decimal)', value=0.0)  # decimal
+upper_reservoir_level = st.number_input('Upper Reservoir Level (m)', value=10000.0)  # meters
+lower_reservoir_level = st.number_input('Lower Reservoir Level (m)', value=2000.0)  # meters
+upper_reservoir_volume = st.number_input('Upper Reservoir Volume (cubic meters)', value=120000.0)  # cubic meters
+lower_reservoir_volume = st.number_input('Lower Reservoir Volume (cubic meters)', value=40000.0)  # cubic meters
+pump_efficiency = st.number_input('Pump Efficiency (decimal)', value=0.8)  # decimal
+generator_efficiency = st.number_input('Generator Efficiency (decimal)', value=0.9)  # decimal
 
 if st.button('Calculate'):
     # calculate the net head and overall efficiency
@@ -31,11 +31,21 @@ if st.button('Calculate'):
     available_energy = ((upper_reservoir_volume - lower_reservoir_volume) * net_head * water_density * gravity * overall_efficiency) / 3600 / 1000 # MWh
 
     # print variables and results for debugging
-    st.write(f"Input variables:\nUpper reservoir level: {upper_reservoir_level} m\nLower reservoir level: {lower_reservoir_level} m\nUpper reservoir volume: {upper_reservoir_volume} m³\nLower reservoir volume: {lower_reservoir_volume} m³\nPump efficiency: {pump_efficiency}\nGenerator efficiency: {generator_efficiency}")
-    st.write(f"Calculations:\nNet head: {net_head} m\nOverall efficiency: {overall_efficiency}\nAvailable energy: {available_energy} MWh")
+    st.markdown(f"### Input variables:")
+    st.markdown(f"- Upper reservoir level: {upper_reservoir_level} m")
+    st.markdown(f"- Lower reservoir level: {lower_reservoir_level} m")
+    st.markdown(f"- Upper reservoir volume: {upper_reservoir_volume} m³")
+    st.markdown(f"- Lower reservoir volume: {lower_reservoir_volume} m³")
+    st.markdown(f"- Pump efficiency: {pump_efficiency}")
+    st.markdown(f"- Generator efficiency: {generator_efficiency}")
+
+    st.markdown(f"### Calculations:")
+    st.markdown(f"- Net head: {net_head} m")
+    st.markdown(f"- Overall efficiency: {overall_efficiency}")
+    st.markdown(f"- Available energy: {available_energy} MWh")
 
     # format available energy to one decimal place
     formatted_energy = "{:.1f}".format(available_energy)
 
     # display the available energy
-    st.write(f'Available Energy: {formatted_energy} MWh')
+    st.markdown(f'### Available Energy: {formatted_energy} MWh')
